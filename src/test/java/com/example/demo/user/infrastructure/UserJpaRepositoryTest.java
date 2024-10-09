515abc67a01e14a2e1fe3362a3e9fec60c376bfd
doydoit <doydoit@gmail.com>
2024-10-10 07:07:20 +0900

    섹션 5 - 2부: 실기 수업 - 외부 연동을 다루는 방법
    - Repository 의존성 역전 적용

--- src/test/java/com/example/demo/user/infrastructure/UserJpaRepositoryTest.java
+++ src/test/java/com/example/demo/user/infrastructure/UserJpaRepositoryTest.java
@@ -12,32 +12,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest(showSql = true)
 @Sql("/sql/user-repository-test-data.sql")
-class UserRepositoryTest {
+class UserJpaRepositoryTest {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserJpaRepository userJpaRepository;
 
     @Test
     void findByIdAndStatus로_유저_데이터를_찾을_수_있다() {
-        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
+        Optional<UserEntity> result = userJpaRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
         assertThat(result.isPresent()).isTrue();
     }
 
     @Test
     void findByIdAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
-        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.PENDING);
+        Optional<UserEntity> result = userJpaRepository.findByIdAndStatus(1, UserStatus.PENDING);
         assertThat(result.isEmpty()).isTrue();
     }
 
     @Test
     void findByEmailAndStatus_로_찾을_수_있다() {
-        Optional<UserEntity> result = userRepository.findByEmailAndStatus("doydoit@gmail.com", UserStatus.ACTIVE);
+        Optional<UserEntity> result = userJpaRepository.findByEmailAndStatus("doydoit@gmail.com", UserStatus.ACTIVE);
         assertThat(result.isPresent()).isTrue();
     }
 
     @Test
     void findByEmailAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
-        Optional<UserEntity> result = userRepository.findByEmailAndStatus("doydoit@gmail.com", UserStatus.PENDING);
+        Optional<UserEntity> result = userJpaRepository.findByEmailAndStatus("doydoit@gmail.com", UserStatus.PENDING);
         assertThat(result.isEmpty()).isTrue();
     }
 }
